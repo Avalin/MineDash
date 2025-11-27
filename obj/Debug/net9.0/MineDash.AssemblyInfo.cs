07c62b113a94db869a87f6b4cc40f318d4a2f993
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2025-11-27 01:40:17 +0100

    Fixed timed commands interface

--- obj/Debug/net9.0/MineDash.AssemblyInfo.cs
+++ obj/Debug/net9.0/MineDash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MineDash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff15927db99909844e6b8e53179caf1828f0aea8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+421b8c3bf25ecc3acd74fa894a439c3650e90d37")]
 [assembly: System.Reflection.AssemblyProductAttribute("MineDash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MineDash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
