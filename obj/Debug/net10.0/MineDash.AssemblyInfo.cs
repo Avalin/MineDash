3129f20bc490ff3e1ca9381a7cf2a5ac72aca333
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2026-07-08 18:46:31 +0200

    -

--- obj/Debug/net10.0/MineDash.AssemblyInfo.cs
+++ obj/Debug/net10.0/MineDash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MineDash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba1b7af37a281007c6680273fbf0ddf17604e910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa345a37184775d03d45ef76c60875c4022ba1bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MineDash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MineDash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
